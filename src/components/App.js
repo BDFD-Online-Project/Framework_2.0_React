c0329e7b628f92ee3ac1b5456b3638735106f5f2
BDFD <bdfd2005@gmail.com>
2020-04-22 20:00:08 -0400

    1

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
-import React, { Component } from "react";
-import Countdown from "./Countdown/index.js";
+import React from "react";
+import Countdown from "./Countdown/";
 // import Test from './Test';
 
+/* the class component is slower than function component
+   and we can switch with those two mode 
 export class App extends Component {
   render() {
     return <Countdown />;
   }
 }
+*/
+
+const App = (props) => <Countdown />;
 
 export default App;
